a7f55437a091c14c9c3afa4caff19ff366f8e4b2
John Lancaster <john.k.lancaster@gmail.com>
2012-10-28 10:04:58 -0400

    added CSS 2.1 pseudo classes, updated tests

--- pseudio.js
+++ pseudio.js
@@ -1,8 +1,19 @@
 exports.pseudio = function (selector) {
   var parens = "\(([^\)]+)\)";
 
-  /* obtained from: http://reference.sitepoint.com/css/css3psuedoclasses */
-  var pseudoClasses = [
+  /* (CSS2.1) obtained from http://www.w3.org/TR/CSS2/selector.html */
+  var pseudoClasses2_1 = [
+    ":first-child",
+    ":link",
+    ":visited",
+    ":active",
+    ":hover",
+    ":focus",
+    ":lang" + parens
+  ];
+
+  /* (CSS3) obtained from: http://reference.sitepoint.com/css/css3psuedoclasses */
+  var pseudoClasses3 = [
     ":nth-child" + parens,
     ":nth-last-child" + parens,
     ":nth-of-type" + parens,
@@ -21,7 +32,7 @@ exports.pseudio = function (selector) {
     ":not" + parens
   ];
 
-  /* obtained from: http://reference.sitepoint.com/css/pseudoelement-selection */
+  /* (CSS3) obtained from: http://reference.sitepoint.com/css/pseudoelement-selection */
   var pseudoElements = [
     ":first-letter",
     ":first-line",
@@ -30,7 +41,7 @@ exports.pseudio = function (selector) {
     "::selection"
   ];
 
-  var pseudo = [].concat(pseudoClasses, pseudoElements);
+  var pseudo = [].concat(pseudoClasses2_1, pseudoClasses3, pseudoElements);
 
   // for pseudo classes that take arguments, strip that shit
   var rePseudoClasses = {};
@@ -39,6 +50,7 @@ exports.pseudio = function (selector) {
     rePseudoClasses[":nth-of-type" + parens] = /:nth-of-type\(([^\)]+)\)/;
     rePseudoClasses[":nth-last-of-type" + parens]  = /:nth-last-of-type\(([^\)]+)\)/;
     rePseudoClasses[":not" + parens] = /:not\(([^\)]+)\)/;
+    rePseudoClasses[":lang" + parens] = /:lang\(([^\)]+)\)/;
 
   pseudo.forEach(function (pclass) {
     var re = rePseudoClasses[pclass] === undefined ? pclass : rePseudoClasses[pclass],
